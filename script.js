ac4d0f41c5171c6ef59de82c50af16cc1a8841d8
Victor Henrique <123006884+victorhnbas@users.noreply.github.com>
2023-06-06 20:52:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+$("img").attr("draggable","false")
+
+function jap(){
+    var palavraA = document.getElementById("trad");
+        palavraA.innerHTML = "翻訳"
+    var palavraB = document.getElementById("trad1");
+        palavraB.innerHTML = "翻訳"
+    var palavraC = document.getElementById("pTranslate");
+        palavraC.innerHTML = "<a>JavaScript</a> を簡単なプロジェクトに実装して、<a>JavaScript</a> の使用法をトレーニングするプロジェクト。"
+    var palavraD = document.getElementById("hOME");
+        palavraD.innerHTML = "始める"
+    }
+function chi(){
+    var palavraB = document.getElementById("trad");
+        palavraB.innerHTML = "翻譯"
+    var palavraB = document.getElementById("trad1");
+        palavraB.innerHTML = "翻譯"
+    var palavraC = document.getElementById("pTranslate");
+        palavraC.innerHTML = "項目訓練<a>JavaScript</a>的使用，在一個簡單的項目中實現它。"
+    var palavraD = document.getElementById("hOME");
+        palavraD.innerHTML = "開始"
+    }
+function cor(){
+    var palavraC = document.getElementById("trad");
+        palavraC.innerHTML = "번역하다"
+    var palavraB = document.getElementById("trad1");
+        palavraB.innerHTML = "번역하다"
+    var palavraC = document.getElementById("pTranslate");
+        palavraC.innerHTML = "간단한 프로젝트에서 <a>JavaScript</a>를 구현하여 JavaScript 사용을 교육하는 프로젝트입니다."
+    var palavraD = document.getElementById("hOME");
+        palavraD.innerHTML = "시작"
+    }
+function esp(){
+    var palavraD = document.getElementById("trad");
+        palavraD.innerHTML = "Traducir."
+    var palavraB = document.getElementById("trad1");
+        palavraB.innerHTML = "Traducir."
+    var palavraC = document.getElementById("pTranslate");
+        palavraC.innerHTML = "Proyecto para entrenar el uso de <a>JavaScript</a>, implementándolo en un proyecto simple."
+    var palavraD = document.getElementById("hOME");
+        palavraD.innerHTML = "comenzar"
+    }
+function ale(){
+    var palavraE = document.getElementById("trad");
+        palavraE.innerHTML = "Übersetzen."
+    var palavraB = document.getElementById("trad1");
+        palavraB.innerHTML = "Übersetzen."
+    var palavraC = document.getElementById("pTranslate");
+        palavraC.innerHTML = "Projekt zum Trainieren der Verwendung von <a>JavaScript</a> und dessen Implementierung in einem einfachen Projekt."
+    var palavraD = document.getElementById("hOME");
+        palavraD.innerHTML = "Start"
+    }
+function fra(){
+    var palavraF = document.getElementById("trad");
+        palavraF.innerHTML = "Traduire."
+    var palavraB = document.getElementById("trad1");
+        palavraB.innerHTML = "Traduire."
+    var palavraC = document.getElementById("pTranslate");
+        palavraC.innerHTML = "Projet pour former à l'utilisation de <a>JavaScript</a>, en l'implémentant dans un projet simple."
+    var palavraD = document.getElementById("hOME");
+        palavraD.innerHTML = "commencer"
+    }
+function ing(){
+    var palavraG = document.getElementById("trad");
+        palavraG.innerHTML = "Translate."
+    var palavraB = document.getElementById("trad1");
+        palavraB.innerHTML = "Translate."
+    var palavraC = document.getElementById("pTranslate");
+        palavraC.innerHTML = "Project to train the use of <a>JavaScript</a>, implementing it in a simple project."
+    var palavraD = document.getElementById("hOME");
+        palavraD.innerHTML = "Home"
+    }
+function ita(){
+    var palavraH = document.getElementById("trad");
+        palavraH.innerHTML = "Tradurre."
+    var palavraB = document.getElementById("trad1");
+        palavraB.innerHTML = "Tradurre."
+    var palavraC = document.getElementById("pTranslate");
+        palavraC.innerHTML = "Progetto per addestrare all'uso di <a>JavaScript</a>, implementandolo in un semplice progetto."
+    var palavraD = document.getElementById("hOME");
+        palavraD.innerHTML = "inizio"
+    }
+function por(){
+    var palavraI = document.getElementById("trad");
+        palavraI.innerHTML = "Traduzir."
+    var palavraB = document.getElementById("trad1");
+        palavraB.innerHTML = "Traduzir."
+    var palavraC = document.getElementById("pTranslate");
+        palavraC.innerHTML = "Projeto para treinar o uso do <a>JavaScript</a>, implementando em um projeto simples."
+    var palavraD = document.getElementById("hOME");
+        palavraD.innerHTML = "Início"
+    }
